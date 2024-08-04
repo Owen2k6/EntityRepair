fb31e9a9668dccf04a66899303ef8659166ef166
Owen2k6 <owen@owen2k6.com>
2024-08-04 18:52:07 +0100

    Magic 2.0

--- src/com/owen2k6/entityrepair/EntityRepair.java
+++ src/com/owen2k6/entityrepair/EntityRepair.java
@@ -1,9 +1,10 @@
 package com.owen2k6.entityrepair;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
 
 public class EntityRepair extends JavaPlugin {
     @Override
@@ -20,6 +21,7 @@ public class EntityRepair extends JavaPlugin {
     public void updateEnt() {
         for (Player p : Bukkit.getOnlinePlayers()) {
                 for (Player op : Bukkit.getOnlinePlayers()) {
+                    op.hidePlayer(p);
                     op.showPlayer(p);
             }
         }
